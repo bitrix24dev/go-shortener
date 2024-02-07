74e67294e31e3211670cd7008067d276075174e0
Dmitri07 <dyuosetskiy@rt-dc.ru>
2024-02-07 17:57:47 +0300

    Первый коммит

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleURLShortening(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	shortenedURL := "http://localhost:8080/EwHXdJfB"
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, shortenedURL)
+}
+
+func handleRedirect(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	originalURL := "https://practicum.yandex.ru/"
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/{id}`, handleRedirect)
+	mux.HandleFunc(`/`, handleURLShortening)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
